20f3f3e3e10b053f71abbbf5835cdfb7b1450a4e
Marina Yurkevich <marina.yurkev@gmail.com>
2020-10-13 15:46:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array && array.length !== 0) {
+    return Math.min.apply(Math, array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length !== 0) {
+    return Math.max.apply(Math, array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length !== 0) {
+    return array.reduce((total, item) => total + item, 0) / array.length;
+  }
   return 0;
 }
